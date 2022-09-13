b8b713d7ad5941e4ee0cb41e8c9365ebbf6072b9
LKC <lkcminh@gmail.com>
2022-09-13 09:32:17 +0700

    done css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Trivia from "./components/Trivia";
+
+const moneyPyramid = [
+    { id: 15, amount: "$ 1000000" },
+    { id: 14, amount: "$ 500000" },
+    { id: 13, amount: "$ 250000" },
+    { id: 12, amount: "$ 125000" },
+    { id: 11, amount: "$ 64000" },
+    { id: 10, amount: "$ 32000" },
+    { id: 9, amount: "$ 16000" },
+    { id: 8, amount: "$ 8000" },
+    { id: 7, amount: "$ 4000" },
+    { id: 6, amount: "$ 2000" },
+    { id: 5, amount: "$ 1000" },
+    { id: 4, amount: "$ 500" },
+    { id: 3, amount: "$ 300" },
+    { id: 2, amount: "$ 200" },
+    { id: 1, amount: "$ 100" },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [questionNumber, setQuestionNumber] = useState(15);
+
+    return (
+        <div className="App">
+            <div className="main">
+                <div className="top">
+                    <div className="timer">30</div>
+                </div>
+                <div className="bottom">
+                    <Trivia />
+                </div>
+            </div>
+            <div className="pyramid">
+                <ul className="moneyList">
+                    {moneyPyramid.map((item) => (
+                        <li
+                            className={
+                                questionNumber === item.id
+                                    ? "moneyListItem active"
+                                    : "moneyListItem"
+                            }
+                            key={item.id}
+                        >
+                            <span className="moneyListItemNumber">{item.id}</span>
+                            <span className="moneyListItemAmount">{item.amount}</span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Trivia.js
@@ -0,0 +1,15 @@
+function Trivia() {
+    return (
+        <div className="Trivia">
+            <div className="question">Who's the best handsome guy?</div>
+            <div className="answers">
+                <div className="answer">Minh</div>
+                <div className="answer">Nguyen Trong Minh</div>
+                <div className="answer">Minh Tam</div>
+                <div className="answer correct">Minh LKC</div>
+            </div>
+        </div>
+    );
+}
+
+export default Trivia;
